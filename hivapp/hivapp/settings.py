b895c611c4029d76183f8cda572b802defe85734
marksgy <331677407@qq.com>
2017-11-09 13:29:57 +0800

    Update settings.py

--- hivapp/hivapp/settings.py
+++ hivapp/hivapp/settings.py
@@ -132,9 +132,9 @@ STATIC_URL = './static/'
 STATIC_ROOT ='./static/'
 
 # weixinapp addtion
-WXAPP_ID = 'wxc066d762e4dd7357'
+WXAPP_ID = ''
 
-WXAPP_SECRET = '7bba3ab00edb1c153139f3d75a0020ed'
+WXAPP_SECRET = ''
 
 WXAPP_TOKE = ''
 GRANT_TYPE = 'authorization_code'
